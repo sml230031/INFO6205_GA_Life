6290ca4bae2b49868a1cb434eb59dbd05d776cef
Myron Song <song.man@husky.neu.edu>
2019-12-01 15:18:31 -0500

    Initial

---
+++ src/main/java/edu/neu/coe/info6205/life/GA/GA.java
@@ -0,0 +1,49 @@
+package edu.neu.coe.info6205.life.GA;
+
+import edu.neu.coe.info6205.life.base.Game;
+import edu.neu.coe.info6205.life.base.Point;
+import io.jenetics.*;
+import io.jenetics.engine.*;
+import io.jenetics.util.IntRange;
+import io.jenetics.engine.Limits.*;
+import static io.jenetics.engine.EvolutionResult.toBestPhenotype;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+
+
+
+public class GA {
+
+
+
+    public static Long eval(int[][] gt){
+
+        Game game = new Game();
+        List<Point> points = new ArrayList<>();
+        for(int i=0; i < gt.length; i++)
+            for(int j=0; j < gt[i].length; j++){
+//                System.out.println(gt[i][j]);
+                if (gt[i][j] == 1){
+                    points.add(new Point(i,j));
+                }
+            }
+        return Game.run(0L, points, 10).generation;
+
+    }
+    public static void main(String[] args) {
+        final Engine<IntegerGene, Long> engine = Engine.builder(GA::eval, Codecs.ofMatrix(IntRange.of(0, 1), 3, 3)).optimize(Optimize.MAXIMUM).populationSize(500).alterers(
+                new Mutator<>(0.2),
+                new MeanAlterer<>(0.35)).build();
+
+        final EvolutionStatistics<Integer, ?> statistics = EvolutionStatistics.ofNumber();
+
+        final Phenotype<IntegerGene, Long> result = engine.stream().limit(Limits.bySteadyFitness(7)).limit(10).collect(toBestPhenotype());
+
+        System.out.println(result);
+    }
+}
+
---
+++ src/main/java/edu/neu/coe/info6205/life/base/Point.java
@@ -0,0 +1,171 @@
+package edu.neu.coe.info6205.life.base;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.UnaryOperator;
+
+public class Point implements Comparable<Point> {
+
+		public int getX() {
+				return x;
+		}
+
+		public int getY() {
+				return y;
+		}
+
+		private final int x;
+		private final int y;
+
+		public Point(int x, int y) {
+				this.x = x;
+				this.y = y;
+		}
+
+		public Point copy() {
+				return new Point(getX(), getY());
+		}
+
+		Point transpose() {
+				return new Point(getY(), getX());
+		}
+
+		public Point map(UnaryOperator<Point> f) {
+				return f.apply(this);
+		}
+
+		public Point move(int x, int y) {
+				return new Point(this.x + x, this.y + y);
+		}
+
+		public Point move(Point p) {
+				return move(p.x, p.y);
+		}
+
+		public Point relative(Point point) {
+				return move(-point.getX(), -point.getY());
+		}
+
+		/**
+		 * Return the vector from this Point to other.
+		 *
+		 * @param other the other point.
+		 * @return a Point representing a vector which, if applied to this as a move, would result in "other".
+		 */
+		public Point vector(Point other) {
+				return (new Point(other.getX() - this.x, other.getY() - this.y));
+		}
+
+		public boolean valid() {
+				return x > 0 && y > 0;
+		}
+
+		/**
+		 * Method to compare two points
+		 *
+		 * @param p the other point.
+		 * @return as follows:
+		 * 0: points are the same; otherwise:
+		 * 1: p is due East of this;
+		 * 3: p is due North of this;
+		 * -1: p is due West of this;
+		 * -3: p is due South of this;
+		 * 4: p is NE of this;
+		 * 2: p is NW of this;
+		 * -4: p is SW of this;
+		 * -2: p is SE of this;
+		 */
+		public int compare(Point p) {
+				int cfX = Integer.compare(p.getX(), getX());
+				int cfY = Integer.compare(p.getY(), getY());
+				return cfX + 3 * cfY;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+				if (this == o) return true;
+				if (o == null || getClass() != o.getClass()) return false;
+				Point point = (Point) o;
+				return x == point.x &&
+								y == point.y;
+		}
+
+		@Override
+		public int hashCode() {
+				return Objects.hash(x, y);
+		}
+
+		@Override
+		public String toString() {
+				return "{" +
+								x +
+								", " + y +
+								'}';
+		}
+
+		public static List<Point> points(String s) {
+				List<Point> result = new ArrayList<>();
+				for (String w : s.split(", *")) result.add(point(w));
+				return result;
+		}
+
+		/**
+		 * Compares this Point with other.
+		 * The basis of comparison is the distance to the origin.
+		 *
+		 * @param other the Point to be compared.
+		 * @return a negative integer, zero, or a positive integer as this object
+		 * is less than, equal to, or greater than the specified object.
+		 * @throws NullPointerException if the specified object is null
+		 * @throws ClassCastException   if the specified object's type prevents it
+		 *                              from being compared to this object.
+		 */
+		@Override
+		public int compareTo(Point other) {
+				return Double.compare(this.distance(), other.distance());
+		}
+
+		/**
+		 * Method to generate a point vector based on direction and length.
+		 *
+		 * @param direction 0: SW, 1: S, 2: SE, 3: W, 4: E, 5: NW, 6: N, 7: NE.
+		 * @param length the length of the vector.
+		 * @return a point vector in the given direction with the given length.
+		 */
+		public static Point createVector(int direction, int length) {
+//				System.out.println("createVector: "+direction+", "+length);
+				if (direction < 0 || direction > 7) throw new LifeException("createVector: invalid direction: " + direction);
+				final int n = direction - 3;
+				if (n > 0) {
+						int y = n > 1 ? 1 : 0;
+						int x = n == 2 ? -1 : n == 3 ? 0 : 1;
+						return new Point(x * length, y * length);
+				} else return createVector(7 - direction, -length);
+		}
+
+		/**
+		 * Calculate the distance this point is from the origin.
+		 *
+		 * @return the square root of x^2 + y^2.
+		 */
+		private double distance() {
+				return Math.sqrt(x * x + y * y);
+		}
+
+		/**
+		 * Method to create a point from a String.
+		 *
+		 * @param s a String representing a coordinate pair (x first) separated by a space.
+		 * @return a new Point corresponding to the given String s.
+		 * @throws NumberFormatException          if one of the coordinates cannot be parsed as an integer.
+		 * @throws ArrayIndexOutOfBoundsException if the string cannot be split into two sub-strings.
+		 */
+		private static Point point(String s) {
+				String[] ws = s.split(" ");
+				int x = Integer.parseInt(ws[0]);
+				int y = Integer.parseInt(ws[1]);
+				return new Point(x, y);
+		}
+}
+
